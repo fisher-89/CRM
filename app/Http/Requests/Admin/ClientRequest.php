1503b3570b060bf29465a92599d70af1dcdac644
354226867 <354226867@qq.com>
2018-08-28 14:00:37 +0800

    fix:bug修正

--- app/Http/Requests/Admin/ClientRequest.php
+++ app/Http/Requests/Admin/ClientRequest.php
@@ -30,64 +30,65 @@ class ClientRequest extends FormRequest
         return [
             'name' => 'required|max:10',
             'source_id' => 'required|numeric|max:5|exists:source,id',
-            'status' => ['required','max:2','numeric',function($attribute, $value, $event){
-                if($value != '-1' && $value != '0' && $value != '1' && $value != '2'){
+            'status' => ['required', 'max:2', 'numeric', function ($attribute, $value, $event) {
+                if ($value != '-1' && $value != '0' && $value != '1' && $value != '2') {
                     return $event('未知状态');
                 }
             }],
-            'gender' => ['required','max:1',function($attribute, $value, $event){
-                if($value != '男' && $value != '女'){
+            'gender' => ['required', 'max:1', function ($attribute, $value, $event) {
+                if ($value != '男' && $value != '女') {
                     return $event('性别不正确');
                 }
             }],
-            'mobile' => ['required','max:14','min:11',
-                function($attribute, $value, $event){
-                    $id=$this->route('id');
-                    if(isset($id)){
-                        $mobileNot=DB::table('clients')->where('mobile',$value)->whereNotIn('id',explode(' ',$id))->first();
-                        if(true===(bool)$mobileNot){
+            'mobile' => ['required', 'max:14', 'min:11',
+                function ($attribute, $value, $event) {
+                    $id = $this->route('id');
+                    if (isset($id)) {
+                        $mobileNot = DB::table('clients')->where('mobile', $value)->whereNotIn('id', explode(' ', $id))->first();
+                        if (true === (bool)$mobileNot) {
                             return $event('电话号码已经存在');
                         }
-                    }else{
-                        $mobile=DB::table('clients')->where('mobile',$value)->first();
-                        if(true===(bool)$mobile){
+                    } else {
+                        $mobile = DB::table('clients')->where('mobile', $value)->first();
+                        if (true === (bool)$mobile) {
                             return $event('电话号码已经存在');
                         }
                     }
                 }
-                ],
+            ],
             'wechat' => 'max:20|nullable',
             'nation' => 'required|max:5|exists:nations,name',
             'id_card_number' => ['required',
-            function($attribute, $value, $event){
-                $id=$this->route('id');
-                if(isset($id)){
-                    $cardNumberNot=DB::table('clients')->where('id_card_number',$value)->whereNotIn('id',explode(' ',$id))->first();
-                    if(true===(bool)$cardNumberNot){
-                        return $event('身份证已经存在');
+                function ($attribute, $value, $event) {
+                    $id = $this->route('id');
+                    if (isset($id)) {
+                        $cardNumberNot = DB::table('clients')->where('id_card_number', $value)->whereNotIn('id', explode(' ', $id))->first();
+                        if (true === (bool)$cardNumberNot) {
+                            return $event('身份证已经存在');
+                        }
+                    } else {
+                        $cardNumber = DB::table('clients')->where('id_card_number', $value)->first();
+                        if (true === (bool)$cardNumber) {
+                            return $event('身份证已经存在');
+                        }
                     }
-                }else{
-                    $cardNumber=DB::table('clients')->where('id_card_number',$value)->first();
-                    if(true===(bool)$cardNumber){
-                        return $event('身份证已经存在');
-                    }
-                }
-            },
+                },
                 'max:18|',
                 'regex:/^[1-9][0-9]{5}(19|20)[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|31)|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}([0-9]|x|X)$/'],
             'native_place' => 'nullable|max:8',
             'present_address' => 'nullable|max:150',
-            'tag_id.*.id' => ['exists:tags,id','numeric','nullable',
-                function($attribute, $value, $event){
-                    if(count($value)==0){
+            'tag_id.*.id' => ['exists:tags,id', 'numeric', 'nullable',
+                function ($attribute, $value, $event) {
+            dd(123);
+                    if (count($value) == 0) {
                         return $event('未选择标签');
                     }
                 }
-                ],
+            ],
             'first_cooperation_at' => 'required|date',
-            'vindicator_sn' => ['numeric','nullable',
+            'vindicator_sn' => ['numeric', 'nullable',
                 function ($attribute, $value, $event) {
-                    if((bool)$value===true){
+                    if ((bool)$value === true) {
                         try {
                             $oa = app('api')->withRealException()->getStaff($value);
                             if ((bool)$oa === false) {
@@ -101,10 +102,10 @@ class ClientRequest extends FormRequest
             ],
             'vindicator_name' => 'max:10',
             'remark' => 'max:200',
-            'brand_id.*.id'=>[
-                'numeric','required'
+            'brand_id.*.id' => [
+                'numeric', 'required'
             ],
-            'shop_id.*.id'=>[
+            'shop_id.*.id' => [
                 'required',
             ]
         ];
@@ -131,6 +132,7 @@ class ClientRequest extends FormRequest
             //品牌  店铺
         ];
     }
+
     public function getData()
     {
 //        $user=$this->user()->staff_sn;
