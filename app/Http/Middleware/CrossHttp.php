e1a35d3e1e77308fa86641232017a038c6fbd842
354226867 <354226867@qq.com>
2018-08-30 11:04:39 +0800

    fix:修改文件上传

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -16,7 +16,7 @@ class CrossHttp
     public function handle($request, Closure $next)
     {
         $response=$next($request);
-        $response->header('Access-Control-Allow-Credentials',true);
+        $response->header('Access-Control-Allow-Credentials',false);
         $response->header('Access-Control-Allow-Headers','Origin, Content-Type, Authorization, Cookie, Accept, multipart/form-data, application/json');
         $response->header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Origin','*');
