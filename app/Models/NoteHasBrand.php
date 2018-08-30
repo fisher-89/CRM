2d1c6255377ed4ac1bff044f01e227c34537f0e7
354226867 <354226867@qq.com>
2018-08-30 15:49:19 +0800

    feat:客户事件加品牌

---
+++ app/Models/NoteHasBrand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\ListScopes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class NoteHasBrand extends Model
+{
+    use ListScopes;
+
+    protected $table = 'note_has_brand';
+
+    protected $fillable = ['note_id','brand_id' ];
+
+}
