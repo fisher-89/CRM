425e8b82b319dfec92655fcef98dae4ef7b24069
354226867 <354226867@qq.com>
2018-09-04 10:18:21 +0800

    feat:新加资源类

---
+++ app/Http/Resources/AuthorityCollection.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AuthorityCollection extends ResourceCollection
+{
+    public static $wrap = null;
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+//        return parent::toArray($request);
+        return $this->collection->map(function ($data) {
+            if((bool)$data->editables === true){
+                foreach ($data->editables as $item){
+                    $editables[]=$item['brand_id'];
+                }
+            }
+            if((bool)$data->visibles === true){
+                foreach ($data->visibles as $items){
+                    $visibles[]=$items['brand_id'];
+                }
+            }
+            return [
+                "id"=> $data->id,
+                "name"=> $data->name,
+                "description" => $data->description,
+                "created_at"=> $data->created_at,
+                "updated_at"=> $data->updated_at,
+                "deleted_at"=> $data->deleted_at,
+                "editables"=> isset($editables) ? $editables : [],
+                "visibles"=> isset($visibles) ? $visibles : [],
+                "staffs"=> $data->staffs
+            ];
+        })->toArray();
+    }
+}
