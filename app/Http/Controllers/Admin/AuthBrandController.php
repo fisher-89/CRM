8dd3b77bf4ffc30b6656ba92be1d2c7d5be98f39
354226867 <354226867@qq.com>
2018-09-05 09:17:56 +0800

    fix:当前员工品牌

---
+++ app/Http/Controllers/Admin/AuthBrandController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Models\AuthorityGroups;
+use Illuminate\Http\Request;
+
+class AuthBrandController extends Controller
+{
+    public function getBrand(Request $request)
+    {
+        $staffSn = $request->user()->staff_sn;
+        $obj = AuthorityGroups::whereHas('staffs', function ($query) use ($staffSn) {
+            $query->where('staff_sn', $staffSn);
+        })->with('editables')->get();
+        foreach ($obj as $k => $v) {
+            foreach ($v['editables'] as $key => $value) {
+                $authData[] = $value['brand_id'];
+            }
+        }
+        $data = isset($authData) ? $authData : [];
+        if ((bool)$data == false) {
+            return [];
+        }
+        return array_unique( array_filter($data));
+    }
+}
