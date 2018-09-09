11316b1ae88a2ef1b540fb12df5567e9d467b65c
354226867 <354226867@qq.com>
2018-09-09 16:25:04 +0800

    fix:bug修复

--- app/Http/Controllers/Admin/NotesController.php
+++ app/Http/Controllers/Admin/NotesController.php
@@ -34,7 +34,7 @@ class NotesController extends Controller
      */
     public function indexType(Request $request)
     {
-        dd($request->getClientIp());
+        dd($request->getHost());
 //        return $this->note->getListType($request);
     }
 
