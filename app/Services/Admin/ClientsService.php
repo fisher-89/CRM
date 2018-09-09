3c7f04d4bea5641fdd2e0a34d70c595ff75c5cfc
354226867 <354226867@qq.com>
2018-09-09 09:50:30 +0800

    fix:bug修复

--- app/Services/Admin/ClientsService.php
+++ app/Services/Admin/ClientsService.php
@@ -127,31 +127,37 @@ class ClientsService
             $this->clientHasTags->where('client_id', $clientData->id)->delete();
             $this->clientHasShops->where('client_id', $clientData->id)->delete();
             $this->clientHasBrands->where('client_id', $clientData->id)->delete();
-            if (isset($request->tags) && $request->tags != []) {
-                foreach ($request->tags as $k => $v) {
-                    $sql = [
-                        'client_id' => $clientData->id,
-                        'tag_id' => $v['tag_id'],
-                    ];
-                    $this->clientHasTags->create($sql);
+            if (isset($request->tags)) {
+                if($request->tags != []){
+                    foreach ($request->tags as $k => $v) {
+                        $sql = [
+                            'client_id' => $clientData->id,
+                            'tag_id' => $v['tag_id'],
+                        ];
+                        $this->clientHasTags->create($sql);
+                    }
                 }
             }
-            if (isset($request->brands) && $request->brands != []) {
-                foreach ($request->brands as $item) {
-                    $brandSql = [
-                        'client_id' => $clientData->id,
-                        'brand_id' => $item['brand_id'],
-                    ];
-                    $this->clientHasBrands->create($brandSql);
+            if (isset($request->brands)) {
+                if($request->brands != []){
+                    foreach ($request->brands as $item) {
+                        $brandSql = [
+                            'client_id' => $clientData->id,
+                            'brand_id' => $item['brand_id'],
+                        ];
+                        $this->clientHasBrands->create($brandSql);
+                    }
                 }
             }
-            if (isset($request->shops) && $request->shops != []) {
-                foreach ($request->shops as $items) {
-                    $shopSql = [
-                        'client_id' => $clientData->id,
-                        'shop_sn' => $items['shop_sn'],
-                    ];
-                    $this->clientHasShops->create($shopSql);
+            if (isset($request->shops)) {
+                if($request->shops != []){
+                    foreach ($request->shops as $items) {
+                        $shopSql = [
+                            'client_id' => $clientData->id,
+                            'shop_sn' => $items['shop_sn'],
+                        ];
+                        $this->clientHasShops->create($shopSql);
+                    }
                 }
             }
             $this->saveClientLog($specialHandling, $all, $request);
