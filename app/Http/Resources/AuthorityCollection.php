a0a4b7661cb2d5b12c12b5474b57fa1002058ba7
354226867 <354226867@qq.com>
2018-09-17 09:18:17 +0800

    fix:bug修复

--- app/Http/Resources/AuthorityCollection.php
+++ app/Http/Resources/AuthorityCollection.php
@@ -7,36 +7,37 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 class AuthorityCollection extends ResourceCollection
 {
     public static $wrap = null;
+
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
 //        return parent::toArray($request);
         return $this->collection->map(function ($data) {
-            if((bool)$data->editables === true){
-                foreach ($data->editables as $item){
-                    $editables[]=$item['brand_id'];
+            if ((bool)$data->editables === true) {
+                foreach ($data->editables as $item) {
+                    $editables[] = $item['brand_id'];
                 }
             }
-            if((bool)$data->visibles === true){
-                foreach ($data->visibles as $items){
-                    $visibles[]=$items['brand_id'];
+            if ((bool)$data->visibles === true) {
+                foreach ($data->visibles as $items) {
+                    $visibles[] = $items['brand_id'];
                 }
             }
             return [
-                "id"=> $data->id,
-                "name"=> $data->name,
+                "id" => $data->id,
+                "name" => $data->name,
                 "description" => $data->description,
-                "created_at"=> $data->created_at->format('Y-m-d H:i:s'),
-                "updated_at"=> $data->updated_at->format('Y-m-d H:i:s'),
-                "deleted_at"=> $data->deleted_at,
-                "editables"=> isset($editables) ? $editables : [],
-                "visibles"=> isset($visibles) ? $visibles : [],
-                "staffs"=> $data->staffs
+                "created_at" => $data->created_at == null ? null : $data->created_at->format('Y-m-d H:i:s'),
+                "updated_at" => $data->updated_at == null ? null : $data->updated_at->format('Y-m-d H:i:s'),
+                "deleted_at" => $data->deleted_at,
+                "editables" => isset($editables) ? $editables : [],
+                "visibles" => isset($visibles) ? $visibles : [],
+                "staffs" => $data->staffs
             ];
         })->toArray();
     }
