ff5092c6cabd21b5ee9d1ba5511a6b0225ec8e57
354226867 <354226867@qq.com>
2018-09-19 16:46:11 +0800

    fix:bug修复

--- app/Http/Resources/ClientLogCollection.php
+++ app/Http/Resources/ClientLogCollection.php
@@ -22,18 +22,18 @@ class ClientLogCollection extends ResourceCollection
         return $this->collection->map(function ($data) {
             return [
                 "id" => $data->id,
-                "client_id" => $data->client_id == null ? '' : $data->client_id,
-                "type" => $data->type == null ? '' : $data->type,
-                "staff_sn" => $data->staff_sn == null ? '' : $data->staff_sn,
-                "staff_name" => $data->staff_name == null ? '' : $data->staff_name,
-                "operation_address" => $data->operation_address == null ? '' : $data->operation_address,
+                "client_id" => $data->client_id === null ? '' : $data->client_id,
+                "type" => $data->type === null ? '' : $data->type,
+                "staff_sn" => $data->staff_sn === null ? '' : $data->staff_sn,
+                "staff_name" => $data->staff_name === null ? '' : $data->staff_name,
+                "operation_address" => $data->operation_address === null ? '' : $data->operation_address,
                 "changes" => $this->trans($data->changes),
                 "status" => $data->status,
-                "restore_sn" => $data->restore_sn == null ? '' : $data->restore_sn,
-                "restore_name" => $data->restore_name == null ? '' : $data->restore_name,
-                "restore_at" => $data->restore_at == null ? '' : $data->restore_at,
-                "created_at" => $data->created_at == null ? '' : $data->created_at->format('Y-m-d H:i:s'),
-                "updated_at" => $data->updated_at == null ? '' : $data->updated_at->format('Y-m-d H:i:s'),
+                "restore_sn" => $data->restore_sn === null ? '' : $data->restore_sn,
+                "restore_name" => $data->restore_name === null ? '' : $data->restore_name,
+                "restore_at" => $data->restore_at === null ? '' : $data->restore_at,
+                "created_at" => $data->created_at === null ? '' : $data->created_at->format('Y-m-d H:i:s'),
+                "updated_at" => $data->updated_at === null ? '' : $data->updated_at->format('Y-m-d H:i:s'),
                 "clients" => $data->clients,
             ];
         })->toArray();
