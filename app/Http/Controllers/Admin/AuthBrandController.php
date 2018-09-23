a05beb9a4c6cd780644eea7df0d155e6eca285d5
354226867 <354226867@qq.com>
2018-09-23 09:49:52 +0800

    fix:bug修复

--- app/Http/Controllers/Admin/AuthBrandController.php
+++ app/Http/Controllers/Admin/AuthBrandController.php
@@ -10,10 +10,10 @@ class AuthBrandController extends Controller
     public function getBrand(Request $request)
     {
         if($request->user()->staff_sn == 999999){
-            return [
+            dd( [
                 "editable"=>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
                 "visible"=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-            ];
+            ]);
         }
         return ['editable'=>$this->getBrandEditable($request),'visible'=>$this->getBrandVisible($request)];
     }
