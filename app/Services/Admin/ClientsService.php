8a874cc3115417ebd5ed6508e70d03af56b7415a
354226867 <354226867@qq.com>
2018-09-25 09:51:40 +0800

    fix:bug修复

--- app/Services/Admin/ClientsService.php
+++ app/Services/Admin/ClientsService.php
@@ -455,7 +455,7 @@ class ClientsService
             }else if (strlen($res[$i][0]) < 6) {
                 $err[$res[$i][0]][] = '名字过短';
             }
-            if(!preg_match('/^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$/',$res[$i][0])){
+            if(!preg_match('/^[\x{4e00}-\x{9fa5}]{2,10}$|^[a-zA-Z\s]*[a-zA-Z\s]{2,20}$/isu',$res[$i][0])){
                 $err[$res[$i][0]][] = '名字格式不正确';
             }
             if (empty($res[$i][1])) {
