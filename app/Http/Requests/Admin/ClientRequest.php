23b3b1245dd532aef9cf49a40b306d7200279e45
354226867 <354226867@qq.com>
2018-09-25 11:26:36 +0800

    fix:bug修复

--- app/Http/Requests/Admin/ClientRequest.php
+++ app/Http/Requests/Admin/ClientRequest.php
@@ -75,7 +75,7 @@ class ClientRequest extends FormRequest
                 },
                 'max:18|',
                 'regex:/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$)/'],
-            'native_place' => 'nullable|max:8',
+            'native_place' => 'nullable|max:8|exists:provincial,name',
             'present_address' => 'nullable|max:150',
             'tags'=>'array|nullable',
             'tags.*.tag_id' => ['exists:tags,id', 'numeric', 'nullable'],
