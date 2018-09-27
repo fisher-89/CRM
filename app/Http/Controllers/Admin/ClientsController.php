e40daf404f50208371310b2c54f01d9ab60e6c0c
354226867 <354226867@qq.com>
2018-09-27 10:59:01 +0800

    fix:bug修复

--- app/Http/Controllers/Admin/ClientsController.php
+++ app/Http/Controllers/Admin/ClientsController.php
@@ -152,21 +152,21 @@ class ClientsController extends Controller
     public function example()
     {
         $source = DB::table('source')->get();
-        $sourceData = array_column($source == null ? [] : $source->toArray(),'name');//来源
-        $status = [ '黑名单','潜在客户','合作中','合作完毕'];
+        $sourceData = array_column($source == null ? [] : $source->toArray(), 'name');//来源
+        $status = ['黑名单', '潜在客户', '合作中', '合作完毕'];
         $statusData = $status;
-        $brand = [ '集团公司', '杰尼威尼专卖', 'JV专卖', '利鲨', 'GO', 'GO快销','杰尼威尼2部','利鲨快销','GO加盟','GO批发','GO专卖','杰尼威尼','JV','女装'];
+        $brand = ['集团公司', '杰尼威尼专卖', 'JV专卖', '利鲨', 'GO', 'GO快销', '杰尼威尼2部', '利鲨快销', 'GO加盟', 'GO批发', 'GO专卖', '杰尼威尼', 'JV', '女装'];
         $brandData = $brand;
         $nations = DB::table('nations')->get();
-        $nationsData = array_column($nations == null ? [] : $nations->toArray(),'name');//民族
+        $nationsData = array_column($nations == null ? [] : $nations->toArray(), 'name');//民族
         $tags = DB::table('tags')->get();
-        $tagsData = array_column($tags == null ? [] : $tags->toArray(),'name');//标签
+        $tagsData = array_column($tags == null ? [] : $tags->toArray(), 'name');//标签
         $provincial = DB::table('provincial')->get();
-        $provincialData = array_column($provincial == null ? [] : $provincial->toArray() ,'name');//籍贯
+        $provincialData = array_column($provincial == null ? [] : $provincial->toArray(), 'name');//籍贯
         $max = count(max($sourceData, $statusData, $brandData, $nationsData, $tagsData, $provincialData));
-        $data[] = ['客户来源','客户状态','合作品牌','名族','标签','籍贯'];
+        $data[] = ['客户来源', '客户状态', '合作品牌', '民族', '标签', '籍贯'];
         for ($i = 0; $i < $max; $i++) {
-            $data[]=[
+            $data[] = [
                 isset($sourceData[$i]) ? $sourceData[$i] : '',
                 isset($statusData[$i]) ? $statusData[$i] : '',
                 isset($brandData[$i]) ? $brandData[$i] : '',
@@ -179,29 +179,29 @@ class ClientsController extends Controller
         $cellData[] = ['例：张三', '例：朋友介绍', '例：合作中', '例：杰尼威尼专卖', '例：女', '例：13333333333', '例：weixin', '例：汉族', '例：510111199905065215', 'VIP客户,市代客户（多个标签用英文逗号分开）', '例：四川省', '例：2010-10-10', '例：110105（选填）', '例：备注（选填）'];
         $fileName = '客户资料导入模板';
         $tot = count($cellData);
-        $maxi=$max+1;
-        Excel::create($fileName, function ($excel) use ($cellData,$data,$tot,$maxi) {
-            $excel->sheet('主页', function ($sheet) use ($cellData,$tot) {
+        $maxi = $max + 1;
+        Excel::create($fileName, function ($excel) use ($cellData, $data, $tot, $maxi) {
+            $excel->sheet('主页', function ($sheet) use ($cellData, $tot) {
                 $sheet->rows($cellData);
                 $sheet->cells('A1:N1', function ($cells) {
                     $cells->setAlignment('center');
-                    $cells->setFontWeight('bold');
+//                    $cells->setFontWeight('bold');
 //                    $cells->setFontColor('#FFFFFF');
-                    $cells->setBackground('#A6FFA6');
+                    $cells->setBackground('#D2E9FF');
                 });
-                $sheet->cells('A2:N'.$tot, function($cells) {
+                $sheet->cells('A2:N' . $tot, function ($cells) {
                     $cells->setAlignment('center');
                 });
             });
-            $excel->sheet('辅助附页', function ($sheet) use ($data,$maxi) {
+            $excel->sheet('辅助附页', function ($sheet) use ($data, $maxi) {
                 $sheet->rows($data);
                 $sheet->cells('A1:F1', function ($cells) {
                     $cells->setAlignment('center');
-                    $cells->setBackground('#A6FFA6');
+                    $cells->setBackground('#D2E9FF');
 //                    $cells->setFontColor('#FFFFFF');
-                    $cells->setFontWeight('bold');
+//                    $cells->setFontWeight('bold');
                 });
-                $sheet->cells('A2:F'.$maxi, function($cells) {
+                $sheet->cells('A2:F' . $maxi, function ($cells) {
                     $cells->setAlignment('center');
                 });
             });
