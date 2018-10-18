285d6eefece33c52c479a08e5b6735dca7fc4722
354226867 <354226867@qq.com>
2018-10-18 11:43:37 +0800

    fix:bug修复

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(NationTableSeeder::class);
          $this->call(ProincialTableSeeder::class);
+         $this->call(LinkageTableSeeder::class);
     }
 }
