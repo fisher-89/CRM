e059c54d2414b326596fc8d5738b548f7856ca87
354226867 <354226867@qq.com>
2018-10-20 18:15:24 +0800

    fix:bug修复

---
+++ app/Models/Linkages.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\ListScopes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Linkages extends Model
+{
+    use ListScopes, SoftDeletes;
+
+    protected $table = 'linkage';
+
+    protected $fillable = [];
+
+    public $timestamps =false;
+}
